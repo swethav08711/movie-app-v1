e110ec3b2dcb54b23d4137f075bb8480d6ea8bc4
swethav08711 <shwe08711@gmail.com>
2023-01-18 12:30:13 +0530

    create components and finish the template

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,9 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
+import Header from '@/components/Header'
+import Navbar from '@/components/Navbar'
+import Results from '@/components/Results'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -13,7 +16,12 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-     <h1>Hello</h1>
+      {/* Header */}
+      <Header/>
+      {/* Navbar */}
+      <Navbar/>
+      {/* Results */}
+      <Results/>
     </>
   )
 }
