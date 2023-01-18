56e8de1995b6ca1ae57ff746cbeef2c24ca17272
swethav08711 <shwe08711@gmail.com>
2023-01-18 14:03:57 +0530

    create the navbar component and requests

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,6 +1,15 @@
+import request from "@/utils/request";
+import { useRouter } from "next/router";
 
 export default function Navbar() {
+  const router = useRouter()
   return (
-    <div>Navbar</div>
+    <div className=" flex justify-center bg-gray-600 text-gray-200 select-none text-xl lg:text-2xl">
+      {
+        Object.entries(request).map(([key,{title,url}])=>(
+          <h2 onClick={()=> router.push(`?genre=${key}`)} className=" m-6 cursor-pointer hover:text-white active:text-red-400" key={key}>{title}</h2>
+        ))
+      }
+    </div>
   )
 }
