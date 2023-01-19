4128954122731120a1304b1bde8b54518f710a98
swethav08711 <shwe08711@gmail.com>
2023-01-19 14:32:01 +0530

    complete the card component and make the result section response

--- components/Card.js
+++ components/Card.js
@@ -5,7 +5,7 @@ import React from "react";
 export default function Card({ results }) {
   const BASE_URL = "https://image.tmdb.org/t/p/original";
   return (
-    <div>
+    <div className=" p-3 cursor-pointer hover:text-white active:text-red-400 xl:hover:scale-105 transition-transform duration-200">
       <Image
         layout="responsive"
         alt="img"
@@ -13,14 +13,14 @@ export default function Card({ results }) {
         height={100}
         src={BASE_URL + results.backdrop_path}
       />
-      <div>
-        <p className=" truncate">
+      <div className=" p-2">
+        <p className=" truncate text-lg">
 
         {results.overview}
         </p>
-        <h2>{results.title || results.name}</h2>
-        <p>{results.release_date || results.first_air_date} 
-        <ThumbUpIcon className=" h-5"/> {results.vote_count}</p>
+        <h2 className="text-lg font-bold">{results.title || results.name}</h2>
+        <p className="flex items-center">{results.release_date || results.first_air_date} 
+        <ThumbUpIcon className=" h-5 ml-3 mr-1"/> {results.vote_count}</p>
        
       </div>
     </div>
